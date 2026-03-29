f339a0426af35f8a9175c5958ac34508abbadde9
Kaique <kaique@fluxo.tec.br>
2026-03-29 19:39:12 -0300

    Adjust region aws

--- infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "desafio-aquarela"
-    key            = "desafio/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "terraform-lock"
-  }
-}
