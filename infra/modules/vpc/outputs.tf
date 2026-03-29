085da6905fa0cbc3aa261bdcfa9fc88b2453e5c4
Kaique <kaique@fluxo.tec.br>
2026-03-29 19:49:57 -0300

    Adjust vpc in eks

---
+++ infra/modules/vpc/outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "private_subnets" {
+  value = aws_subnet.private[*].id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
+
+output "vpc_cidr" {
+  value = aws_vpc.this.cidr_block
+}
