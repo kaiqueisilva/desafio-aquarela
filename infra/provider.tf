177904d6f2c4a5bbbfa1c8212019ae1949fba2cc
Kaique <kaique@fluxo.tec.br>
2026-03-30 10:30:27 -0300

    add iam user

--- infra/provider.tf
+++ infra/provider.tf
@@ -4,9 +4,23 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.10"
+    }
   }
 }
 
 provider "aws" {
   region = var.region
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
